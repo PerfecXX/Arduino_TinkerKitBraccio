2a8ea89726a111fcc20b29871dd765cfdbfbb5c6
Teeraphat Kullanankanjana <watit2013@gmail.com>
2024-10-03 10:01:17 +0700

    Control Robot Movement via Arduino Cloud
    Control Robot Movement via Arduino Cloud

---
+++ example/Board/Arduino UNO R4 WiFi/Arduino Cloud/01-ArmControlWithCloud.ino
@@ -0,0 +1,26 @@
+/*
+Author      : Teeraphat Kullanankanjana
+Version     : 1.0
+Date        : 03/10/2567
+Description : Control TinkerKit Braccio Robot with Arduino Cloud 
+Copyright (C) 2024 Teeraphat Kullanankanjana. All rights reserved.
+*/
+
+// Include the Braccio and Servo library
+#include <Braccio.h>
+#include <Servo.h>
+
+// Set Servo Instance
+Servo base;       // M1
+Servo shoulder;   // M2
+Servo elbow;      // M3
+Servo wrist_ver;  // M4
+Servo wrist_rot;  // M5
+Servo gripper;    // M6
+
+void setup() {
+  Braccio.begin();
+}
+
+void loop() {
+}
